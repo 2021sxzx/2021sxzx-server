7560da9908c9133b5a5f8fbb97bade47302d2391
UmyNya <1079102275@qq.com>
2022-01-17 16:09:19 +0800

    删除提交错了的文件夹

---
+++ routes/role.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router();
+const {
+    addRole,
+    deleteRole,
+    updateRole,
+    getRole,
+    searchRole
+} = require("../controller/roleController")
+
+const {
+    getPermissions,
+    updatePermissions
+} = require("../controller/permissionsController")
+
+function setStatusCode(res, data) {
+    if (data.code === 200) {
+        res.statusCode = 200
+    } else {
+        res.statusCode = 404
+    }
+}
+
+/* 角色和权限相关的路由处理. */
+
+/**
+ * 更新角色非权限相关的信息
+ */
+router.post('/v1/role', async (req, res, next) => {
+    let roleData = req.body;
+    // commentData.create_time = Date.now()
+    let data = await saveRole(roleData);
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+/**
+ * 获取角色信息
+ */
+router.get('/v1/role', async (req, res, next) => {
+    let roleData = req.query
+    let data = await getRole(roleData)
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+/**
+ * 增加角色
+ */
+router.get('/v1/role', async (req, res, next) => {
+    let roleData = req.query
+    let data = await addRole(roleData)
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+/**
+ * 搜索角色
+ */
+router.post('/v1/searchRole', async (req, res, next) => {
+    let searchData = req.body
+    let data = await searchRole(searchData)
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+/**
+ * 获取角色权限
+ */
+router.get('/v1/rolePermissions', async (req, res, next) => {
+    let roleData = req.query
+    let data = await getPermissions(roleData)
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+/**
+ * 更新角色权限
+ */
+ router.post('/v1/rolePermissions', async (req, res, next) => {
+    let roleData = req.query
+    let data = await updatePermissions(roleData)
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+module.exports = router;
