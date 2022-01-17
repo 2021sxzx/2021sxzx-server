5bf3993180a49aff2412f9b6cd82dc1b833fd79c
BK201-Drama <1243532812@qq.com>
2022-01-17 23:13:01 +0800

    feat: 全面完善用户管理路由

--- routes/userManagement.js
+++ routes/userManagement.js
@@ -10,12 +10,38 @@ const {
   searchUserAndReturnList
 } = require('../controller/userManagementController')
 
-router.get('/v1/getUserList', async (req, res, next) => {
-
+router.get('/v1/user/list', async (req, res, next) => {
   const data = await returnUserList();
   res.json(data)
 })
 
-// router.get('/v1/')
+router.post('/v1/user', async (req, res, next) => {
+  const data = await addUserAndReturnList({
+    user_name: req.body.user_name,
+    account: req.body.account,
+    password: req.body.password,
+    role_name: req.body.role_name
+  })
+  res.json(data)
+})
+
+// 修改用户
+router.patch('/v1/user', async (req, res, next) => {
+  const { user_name, password } = req.query
+  const data = await updateUserAndReturnList(user_name, password)
+  res.json(data)
+})
+
+// 删除用户
+router.delete('/v1/user', async (req, res, next) => {
+  const { account } = req.query
+  const data = await deleteUserAndReturnList(account)
+  res.json(data)
+})
+
+router.get('/v1/search', async (req, res, next) => {
+  const { searchValue } = req.query
+  const data = await searchUserAndReturnList(searchValue)
+})
 
 module.exports = router;
