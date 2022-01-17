9f17b5d84a1b7fe39f3731da21080f8d5df02e7d
UmyNya <1079102275@qq.com>
2022-01-17 17:07:23 +0800

    尝试将更新和获取角色的权限信息和非权限信息合并到了一起 ./routes/role.js

--- routes/role.js
+++ routes/role.js
@@ -6,7 +6,7 @@ const {
     updateRole,
     getRole,
     searchRole,
-    updatePermissions
+    updatePermissions// HACK(钟卓江)：感觉可以和其他非权限信息更新合在一起
 } = require("../controller/roleController")
 
 function setStatusCode(res, data) {
@@ -20,12 +20,31 @@ function setStatusCode(res, data) {
 /* 角色和权限相关的路由处理. */
 
 /**
+ * 增加角色
+ */
+ router.get('/v1/role', async (req, res, next) => {
+    let roleData = req.query
+    let data = await addRole(roleData)
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+/**
+ * 删除角色
+ */
+ router.delete('/v1/role', async (req, res, next) => {
+    let roleData = req.query
+    let data = await deleteRole(roleData)
+    setStatusCode(res, data)
+    res.json(data)
+})
+
+/**
  * 更新角色非权限相关的信息
  */
 router.post('/v1/role', async (req, res, next) => {
     let roleData = req.body;
-    // commentData.create_time = Date.now()
-    let data = await saveRole(roleData);
+    let data = await updateRole(roleData);
     setStatusCode(res, data)
     res.json(data)
 })
@@ -41,16 +60,6 @@ router.get('/v1/role', async (req, res, next) => {
 })
 
 /**
- * 增加角色
- */
-router.get('/v1/role', async (req, res, next) => {
-    let roleData = req.query
-    let data = await addRole(roleData)
-    setStatusCode(res, data)
-    res.json(data)
-})
-
-/**
  * 搜索角色
  */
 router.post('/v1/searchRole', async (req, res, next) => {
@@ -60,7 +69,7 @@ router.post('/v1/searchRole', async (req, res, next) => {
     res.json(data)
 })
 
-// XXX（钟卓江）: 可能不需要，可以删掉
+// HACK（钟卓江）: 可能不需要，可以删掉
 // /**
 //  * 获取角色权限
 //  */
@@ -70,15 +79,15 @@ router.post('/v1/searchRole', async (req, res, next) => {
 //     setStatusCode(res, data)
 //     res.json(data)
 // })
-
-/**
- * 更新角色权限
- */
- router.post('/v1/rolePermissions', async (req, res, next) => {
-    let roleData = req.query
-    let data = await updatePermissions(roleData)
-    setStatusCode(res, data)
-    res.json(data)
-})
+// 
+// /**
+//  * 更新角色权限
+//  */
+//  router.post('/v1/rolePermissions', async (req, res, next) => {
+//     let roleData = req.query
+//     let data = await updatePermissions(roleData)
+//     setStatusCode(res, data)
+//     res.json(data)
+// })
 
 module.exports = router;
