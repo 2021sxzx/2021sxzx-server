99d36b1484bd786bf251e95fcfc02ec3ea83e578
BK201-Drama <1243532812@qq.com>
2022-01-18 01:21:44 +0800

    feat: 书写部分用户权限管理架构

--- model/role.js
+++ model/role.js
@@ -5,12 +5,12 @@ const roleSchema = new mongoose.Schema({
   role_name: {   // 角色名称
     type: String,
     unique:[true,'角色名称不能重复'],
-    required: [true,'请传入角色名称，角色名不能为空'],
-    maxlength:[50,'角色名长度不能超过50个字符']
+    required: [true, '请传入角色名称，角色名不能为空'],
+    maxlength:[50, '角色名长度不能超过50个字符']
   },
   role_describe: { // 角色描述
     type: String,
-    maxlength:[500,'角色描述长度不能超过500个字符'],
+    maxlength:[500, '角色描述长度不能超过500个字符'],
     default:'角色描述暂未添加'
   },
   permissions: { // 权限，某个页面的完全访问权限
@@ -39,71 +39,63 @@ const roleSchema = new mongoose.Schema({
      * }
      */
     item_manage: { // 事项管理
-      type: Boolean,
-      default: false
-    },
-    item_manage_process: { // 事项过程管理
-      type: Boolean,
-      default: false
-    },
-    item_manage_guide: { // 事项指南管理
-      type: Boolean,
-      default: false
-    },
-    item_manage_rule: { // 事项规则管理
-      type: Boolean,
-      default: false
+      item_manage_process: { // 事项过程管理
+        type: Boolean,
+        default: false
+      },
+      item_manage_guide: { // 事项指南管理
+        type: Boolean,
+        default: false
+      },
+      item_manage_rule: { // 事项规则管理
+        type: Boolean,
+        default: false
+      }
     },
 
     // XXX(钟卓江)：系统管理这个权限用树形/分级的结构实现会比较好，理由同事项管理那部分一样
     system_manage: { // 系统管理
-      type: Boolean,
-      default: false
-    },
-    system_manage_journal: { // 日志管理，XXX(钟卓江): 叫joural感觉怪怪的，感觉log更贴切
-      type: Boolean,
-      default: false
-    },
-    system_manage_resource: { // 资源管理
-      type: Boolean,
-      default: false
+      system_manage_journal: { // 日志管理，XXX(钟卓江): 叫joural感觉怪怪的，感觉log更贴切
+        type: Boolean,
+        default: false
+      },
+      system_manage_resource: { // 资源管理
+        type: Boolean,
+        default: false
+      },
     },
 
     // XXX(钟卓江)：用户评价管理这个权限用树形/分级的结构实现会比较好，理由同事项管理那部分一样
     comment_manage: { // 用户评价管理
-      type: Boolean,
-      default: false
-    },
-    comment_manage_list: { // 用户评价
-      type: Boolean,
-      default: false
-    },
-    comment_manage_report: { // 用户评价报告
-      type: Boolean,
-      default: false
+      comment_manage_list: { // 用户评价
+        type: Boolean,
+        default: false
+      },
+      comment_manage_report: { // 用户评价报告
+        type: Boolean,
+        default: false
+      }
     },
 
     // TODO(钟卓江)：用户管理这个权限用树形/分级的结构实现会比较好，理由同事项管理那部分一样
     user_manage: { // 用户管理
-      type: Boolean,
-      default: false
-    },
-    user_manage_account: { // 后台账号管理
-      type: Boolean,
-      default: false
-    },
-    user_manage_role: { // 角色管理
-      type: Boolean,
-      default: false
-    },
-    user_manage_department: { // 部门管理
-      type: Boolean,
-      default: false
-    },
-    user_manage_metaData: { // 用户资料元数据管理
-      type: Boolean,
-      default: false
-    },
+      user_manage_account: { // 后台账号管理
+        type: Boolean,
+        default: false
+      },
+      user_manage_role: { // 角色管理
+        type: Boolean,
+        default: false
+      },
+      user_manage_department: { // 部门管理
+        type: Boolean,
+        default: false
+      },
+      user_manage_permission: { // 用户权限管理
+        type: Boolean,
+        default: false
+      }
+    }
   }
 })
 
---
+++ service/roleService.js
@@ -0,0 +1,41 @@
+const role = require('../model/role')
+
+/**
+ * @param role_name
+ * @param role_describe
+ * @param permissions
+ */
+async function addRole () {
+
+}
+/**
+ * 
+ */
+async function getRoleList () {
+
+}
+
+async function updateRole () {
+
+}
+
+async function deleteRole () {
+
+}
+
+async function getRole () {
+
+}
+
+async function calcaulatePermission (PermissionObj) {
+
+} 
+
+module.exports = {
+  addRole,
+  getRoleList,
+  updateRole,
+  deleteRole,
+  getRole,
+  calcaulatePermission
+}
