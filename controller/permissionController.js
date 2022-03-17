37ee6cd7ca36d2d186ac4b0697776513c55034a3
BK201-Drama <1243532812@qq.com>
2022-03-18 02:28:24 +0800

    feat: 补充权限管理

---
+++ controller/permissionController.js
@@ -0,0 +1,26 @@
+const {
+  addPermission,
+  deletePermission,
+  searchPermission,
+  patchPermission
+} = requrie('../service/permissionService');
+const { SuccessModel, ErrorModel } = require('../utils/resultModel')
+
+class permissionController {
+
+  async addPermissionAndReturn (req, res) {
+  
+  }
+
+  async deletePermissionAndReturn (req, res) {
+
+  }
+
+  async searchPermission (req, res) {
+
+  }
+
+  async patchPermission (req, res) {
+
+  }
+}
