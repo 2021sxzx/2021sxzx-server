5ef4a10967cfda80fd422cfe942cb1f0c5a00b9f
lyric <1164778555@qq.com>
2022-04-05 20:20:18 +0800

    Merge branch 'feature/user_management' of github.com:2021sxzx/2021sxzx-server into feature/login

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const jwt = require('jsonwebtoken');
-
 
 const usersRouter = require('./routes/users');
 const commentRouter = require("./routes/comment")
@@ -18,15 +16,16 @@ const userManagementRouter = require('./routes/userManagement')
 const roleRouter = require('./routes/role')
 const sideBarRouter = require('./routes/sideBar');
 
-
 const { MONGO_CONFIG } = require("./config/db") //数据库的配置信息
-
 const mongoose = require("mongoose")
 // const rule = require("./model/rule")
 // const itemRule = require("./model/itemRule")
 // const item = require("./model/item")
 // const itemGuide = require("./model/task")
 mongoose.connect(MONGO_CONFIG.url);
+
+const { validate_jwt } = require('./utils/validateJwt');
+
 const app = express();
 
 // 动态网页的模板设置
@@ -50,32 +49,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /*
 //添加拦截器
-app.use((req, res, next) => {
-  // 验证token
-  const token = req.cookies['auth-token'] || '';
-  // 排除不需要授权的路由
-  if (req.path === '/api/v1/login') {
-    next()
-  } else {
-    if (!token) {
-      res.status(403).send('认证无效，请重新登录。');
-    }
-    else {
-      const result = jwt.verify(token, process.env.JWT_SECRET || 'test');
-      if (result) {
-        req.user = {
-          account: result.account,
-          user_rank: result.user_rank
-        };
-        //console.log(req.user);
-        next();
-      } else {
-        res.status(403).send('认证无效，请重新登录。');
-      }
-    }
-  }
-});
+app.use(validate_jwt);
 */
+
 
 // 处理路由
 app.use('/api', usersRouter);
