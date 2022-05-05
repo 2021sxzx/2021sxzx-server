6ceabb3e0e18ed20cbf07c53304ca3b694f14b44
BK201-Drama <1243532812@qq.com>
2022-05-05 14:06:14 +0800

    feat: 缩短过期时间

--- utils/validateJwt.js
+++ utils/validateJwt.js
@@ -3,8 +3,8 @@ const rediscl = require('../config/redis');
 
 
 const jwt_secret = "jwtfanhere";
-const jwt_expiration = 60 * 60;
-const jwt_refresh_expiration = 60 * 60 * 24 * 30;
+const jwt_expiration = 60 * 10;
+const jwt_refresh_expiration = 60 * 10;
 
 //访问敏感路由时验证用户
 function validate_jwt(req, res, next) {
