afabcfd3226631cb7d6c060316574981c3f4a49e
BK201-Drama <1243532812@qq.com>
2022-06-21 14:45:15 +0800

    UPDATGE

--- service/systemMetaService.js
+++ service/systemMetaService.js
@@ -6,8 +6,10 @@ const { SuccessModel, ErrorModel } = require('../utils/resultModel');
 class systemMetaService {
   constructor () {
     this.userNumber = [];
+    this.lastWeekUserNumber = [];
+    
   }
-
+  
   // 切换db池
   async selectRedisDatabase (db) {
     try {
@@ -22,6 +24,10 @@ class systemMetaService {
     await this.selectRedisDatabase(1);
     try {
       const getUserOnlineNum = await redisClient.dbSize();
+      this.userNumber.push({
+        user: getUserOnlineNum,
+        time: new Date()
+      });
       await this.selectRedisDatabase(0);
       return new SuccessModel({
         msg: '获取在线用户数目成功',
@@ -37,22 +43,33 @@ class systemMetaService {
   }
 
   // 获取当天最大用户数目
-  async getUserOnlineNumberOnThisDay () {
+  async getMaxUserOnlineNumberOnThisDay () {
+    return new SuccessModel({
+      msg: '获取在线用户数目成功',
+      data: this.userNumber.sort((a, b) => b - a)[0]
+    });
+  }
 
+  // 获取当天平均用户数目
+  async getAverageUserOnlineNumberOnThisDay () {
+    const res = this.userNumber.reduce((a, b) => a + b);
+    return new SuccessModel({
+      msg: '获取平均用户数目成功',
+      data: res / this.userNumber.length
+    })
   }
 
   // 测试接口的网络连接质量
   async networkQualityOfInterface (url) {
-    await cp.execFile("ping -c 4 -n", [url], function(err, stdout, stderr){
+    cp.execFile("ping -c 4 -n", [url], function(err, stdout, stderr){
       if(err){console.log(err)}
       const a = iconv.decode(Buffer.from(stdout, 'binary'), 'gbk');
       console.log(stdout);
       console.log(a);
     })
-
     // await cp.exec(`ping ${url}`, { encoding: 'binary' }, function(err, stdout, stderr){
     //   console.log(stdout);
-    //   // console.log(iconv.decode(new Buffer(stdout, 'binary'), 'cp936'));
+    //   console.log(iconv.decode(new Buffer(stdout, 'binary'), 'cp936'));
     // });
   }
 }
