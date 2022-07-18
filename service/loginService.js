3dd55acec4dafdf12315652b43992ba87fa411f6
lhy <944371430@qq.com>
2022-07-18 18:10:18 +0800

    fix(loginService): 删除jwt过期验证
    使用redis验证过期足够，先前版本由于在生成新的token后没有送给cookies，导致浏览器一直保存旧的token，则固定在登录10分钟后就会退出。

--- service/loginService.js
+++ service/loginService.js
@@ -1,152 +1,114 @@
 const User = require('../model/users');
 const jwt = require('jsonwebtoken');
-
-const { 
-  jwt_secret,
-  jwt_expiration,
-  jwt_refresh_expiration,
-  generate_refresh_token,
+const {
+  jwt_secret, jwt_refresh_expiration, generate_refresh_token,
 } = require('../utils/validateJwt');
 
 const redisClient = require('../config/redis')
 
 /**
  * 验证用户身份、生成jwt
- * @param loginData
  * @returns {Promise<*>}
+ * @param db
  */
-
-async function selectRedisDatabase (db) {
+async function selectRedisDatabase(db) {
   try {
     await redisClient.select(db);
   } catch (error) {
-    await selectRedisDatabase (db);
+    await selectRedisDatabase(db);
   }
 }
 
+/**
+ * 登录授权
+ * @param loginData
+ * @returns {Promise<{msg: string, code: number, data: {refresh_token: string, cookie: {httpOnly: boolean}, role_id: *, jwt: {expiresIn: number, token: (*)}, _id: *, unit_id: *, account: *}}|{code: number, message: string}|{msg: string, code: number}|*>}
+ */
 async function authenticate(loginData) {
-    try {
-        const { account, password } = loginData;
-        let res = await User.findOne({ account: account });
-        if (res !== null) {
-            // Compare password
-            if (password === res.password) {
-                if (res.activation_status !== 1) {
-                    return ({ message: '该号码未被激活，请重试.', code: 403 });
-                }
-
-                let refresh_token = generate_refresh_token(64);
-                const maxage = new Date().valueOf();
-                let refresh_token_maxage = new Date(maxage + jwt_refresh_expiration);
-
-                const token = jwt.sign({
-                    account: res.account,
-                }, jwt_secret, {
-                    expiresIn: jwt_expiration
-                });
-
-                // 切换redis数据库至db1
-                await selectRedisDatabase (1)
-                // 将refresh_token保存在redis中
-                await redisClient.set(res.account, JSON.stringify({
-                    refresh_token: refresh_token,
-                    expires: refresh_token_maxage,
-                    token: token
-                }),
-                    redisClient.print
-                );
-                await selectRedisDatabase (0)
-                return ({
-                    msg: 'You have successfully logged in!',
-                    code: 200,
-                    data: {
-                      cookie: {
-                        httpOnly: true
-                      },
-                      jwt: {
-                          token: token,
-                          expiresIn: 3600
-                      },
-                      role_id: res.role_id,
-                      unit_id: res.unit_id,
-                      account: res.account,
-                      _id: res._id,
-                      refresh_token
-                    }
-                });
-            } else {
-                return ({ msg: '密码错误，请重试.', code: 403 });
-            }
-        } else {
-            return ({ msg: '该账号不存在.', code: 403 });
-        }
-    } catch (e) {
-        return e.message;
-    }
+  /**
+   * @property res - 数据库中返回的帐户信息
+   * @property res.account - 帐户帐号
+   * @property res.password - 帐户密码
+   * @property res.activation_status - 帐户激活状态
+   * @property res.role_id
+   * @property res.unit_id
+   * @property res._id
+   */
+  try {
+    const {account, password} = loginData;
+    let res = await User.findOne({account: account});
+    // 错误检查
+    if (res === null) return {msg: '该账号不存在.', code: 403};
+    if (password !== res.password) return {msg: '密码错误，请重试.', code: 403};
+    if (res.activation_status !== 1) return {message: '该号码未被激活，请重试.', code: 403};
+    // 通过错误检查
+    let refresh_token = generate_refresh_token(64);
+    let refresh_token_max_age = new Date(new Date().valueOf() + jwt_refresh_expiration);
+    const token = jwt.sign({account: res.account}, jwt_secret, null, null);
+    // 切换 redis 数据库至 db1
+    await selectRedisDatabase(1)
+    // 将 refresh_token 保存在 redis 中
+    await redisClient.set(res.account, JSON.stringify({
+      refresh_token: refresh_token, expires: refresh_token_max_age, token: token
+    }), redisClient.print);
+    await selectRedisDatabase(0)
+    return {
+      msg: 'You have successfully logged in!', code: 200, data: {
+        cookie: {
+          httpOnly: true
+        }, jwt: {
+          token, expiresIn: 3600
+        }, role_id: res.role_id, unit_id: res.unit_id, account: res.account, _id: res._id, refresh_token
+      }
+    };
+  } catch (e) {
+    return e.message;
+  }
 }
 
 /**
  * 登出
  * @param logoutData
- * @returns {Promise<EnforceDocument<unknown, {}>[]>}
+ * @returns {Promise<number>}
  */
 async function logout(logoutData) {
-    await selectRedisDatabase (1)
-    try {
-        let res = await redisClient.del(logoutData.account);
-        await selectRedisDatabase (0)
-        return res;
-    } catch (e) {
-        await selectRedisDatabase (0)
-        throw new Error(e.message)
-    }
+  await selectRedisDatabase(1)
+  try {
+    let res = await redisClient.del(logoutData.account);
+    await selectRedisDatabase(0)
+    return res;
+  } catch (e) {
+    await selectRedisDatabase(0)
+    throw new Error(e.message)
+  }
 }
 
-// 判断是否达到免密登录标准
-// 如果redis的token过期了，那么就代表免密登录失败
-// 过期时间为600s
-async function isLogin (token) {
-  await selectRedisDatabase (1)
-  const maxage = new Date().valueOf();
-  let refresh_token_maxage = new Date(maxage + jwt_refresh_expiration);
+/**
+ * 判断是否达到免密登录标准，过期时间为600s
+ * @param token 客户端的 token，保存登录账号等信息
+ * @returns {Promise<boolean>} 免密登录是否有效
+ */
+async function isLogin(token) {
+  await selectRedisDatabase(1)
   let refresh_token = generate_refresh_token(64);
-  try {
-    const jw = await jwt.verify(token, jwt_secret);
-    const account = jw.account;
-
-    const thisTime = new Date();
-    let redisData = await redisClient.get(account);
-
-    if (thisTime < new Date(JSON.parse(redisData).expires)) {
-      // 未过期，将redis刷新一下，重新生成一下token并放到cookie
-      const newToken = jwt.sign({
-        account: account
-      }, jwt_secret, {
-        expiresIn: jwt_expiration
-      });
-      redisClient.set(account, JSON.stringify({
-        refresh_token: refresh_token,
-        expires: refresh_token_maxage,
-        token: newToken
-      }), redisClient.print);
-      await selectRedisDatabase (0)
-      return true;
-    } else {
-      await selectRedisDatabase (1)
-      // 已过期，数据用于在router层中做删除cookie的标识
-      await redisClient.del(account);
-      await selectRedisDatabase (0);
-      return false;
-    }
-  } catch (err) {
-    await selectRedisDatabase (0);
-    // 这里判断是否过期
+  const account = await jwt.verify(token, jwt_secret, null, null).account;
+  // 判断当前时间是否小于数据库中的过期时间
+  if (new Date() < new Date(JSON.parse(await redisClient.get(account)).expires)) {
+    // 未过期，将 redis 刷新一下
+    await redisClient.set(account, JSON.stringify({
+      refresh_token: refresh_token, expires: new Date(new Date().valueOf() + jwt_refresh_expiration), token
+    }), redisClient.print);
+    await selectRedisDatabase(0)
+    return true;
+  } else {
+    await selectRedisDatabase(1)
+    // 已过期，数据用于在 router 层中做删除 cookie 的标识
+    await redisClient.del(account);
+    await selectRedisDatabase(0);
     return false;
   }
 }
 
 module.exports = {
-    authenticate,
-    logout,
-    isLogin
+  authenticate, logout, isLogin
 }
