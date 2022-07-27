89ff456f7a6231906ca7216e4937231cf5bc3d04
lhy <944371430@qq.com>
2022-07-27 16:46:44 +0800

    style: 代码格式化

--- routes/systemLog.js
+++ routes/systemLog.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const {
-    getAllSystemLogDetail,
-    getSearchSystemLog,
-    showSystemLogController,
-    getSystemLog2,
-    getItemBrowseCount,
-    getAdvancedSearchSystemLog
-  } = require("../controller/systemLogController")
+  getSearchSystemLog,
+  showSystemLogController,
+  getSystemLog2,
+  getItemBrowseCount,
+  getAdvancedSearchSystemLog
+} = require('../controller/systemLogController')
 
-function setStatusCode(res,data) {
-  if(data.code === 200) {
+function setStatusCode(res, data) {
+  if (data.code === 200) {
     res.statusCode = 200
-  }else {
+  } else {
     res.statusCode = 404
   }
 }
@@ -22,44 +21,44 @@ function setStatusCode(res,data) {
 /**
  * show系统日志的获取
  */
- router.get('/v1/show', async (req,res,next) => {
+router.get('/v1/show', async (req, res, next) => {
   let data = await showSystemLogController()
-  setStatusCode(res,data)
+  setStatusCode(res, data)
   res.json(data)
 })
 
 /**
  * 搜索功能
  */
-router.post('/v1/searchLog',async (req,res,next) => {
+router.post('/v1/searchLog', async (req, res, next) => {
   let searchData = req.body
   let data = await getSearchSystemLog(searchData)
-  setStatusCode(res,data)
+  setStatusCode(res, data)
   res.json(data)
 })
 
 /**
  * 高级搜索功能
  */
- router.post('/v1/advancedSearchLog',async (req,res,next) => {
+router.post('/v1/advancedSearchLog', async (req, res, next) => {
   let searchData = req.body
-  console.log("高级搜索中的参数：",searchData)
+  console.log('高级搜索中的参数：', searchData)
   let data = await getAdvancedSearchSystemLog(searchData)
-  setStatusCode(res,data)
-  res.json(data);
+  setStatusCode(res, data)
+  res.json(data)
 })
 
-router.get('/v1/metaDataLog',async (req,res,next) => {
-    let data = await getSystemLog2()
-    setStatusCode(res,data)
-    res.json(data.data)
+router.get('/v1/metaDataLog', async (req, res, next) => {
+  let data = await getSystemLog2()
+  setStatusCode(res, data)
+  res.json(data.data)
 })
 
-router.get('/v1/itemBrowseCount',async (req,res,next) => {
+router.get('/v1/itemBrowseCount', async (req, res, next) => {
   let data = await getItemBrowseCount()
-  setStatusCode(res,data)
+  setStatusCode(res, data)
   res.json(data.data)
 })
 
 
-module.exports = router;
+module.exports = router
